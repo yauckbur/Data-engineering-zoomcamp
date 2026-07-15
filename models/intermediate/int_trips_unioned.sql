f53cacee568eff91137a44e7c6e74895c59ec11a
yauckbur@gmail.com <yauckbur@gmail.com>
2026-07-15 11:30:26 +0000

    Payment type seed added

--- models/intermediate/int_trips_unioned.sql
+++ models/intermediate/int_trips_unioned.sql
@@ -1,9 +1,13 @@
 WITH green_tripdata AS(
-    SELECT * FROM {{ref('stg_green_tripdata')}}
+    SELECT *,
+        'Green' AS service_type
+     FROM {{ref('stg_green_tripdata')}}
 ),
 
 yellow_tripdata AS (
-    SELECT * FROM {{ref('stg_yellow_tripdata')}}
+    SELECT *,
+         'Yellow' AS service_type
+    FROM {{ref('stg_yellow_tripdata')}}
 ),
 
 trips_unioned AS (
