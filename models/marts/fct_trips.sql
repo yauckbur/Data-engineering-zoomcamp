3f8926d10aa9a7adf2962b63530a0ac0279525f9
Yasin Auckbur <yauckbur@gmail.com>
2026-08-08 16:19:58 +0000

    changed bigquery connection

--- models/marts/fct_trips.sql
+++ models/marts/fct_trips.sql
@@ -1,1 +1,14 @@
-SELECT 1 AS placeholder
+SELECT
+    vendor_id,
+    pickup_datetime,
+    pickup_location_id,
+    service_type,
+    COUNT(*) AS row_count
+FROM {{ ref('int_trips_unioned') }}
+GROUP BY
+    vendor_id,
+    pickup_datetime,
+    pickup_location_id,
+    service_type
+HAVING COUNT(*) > 1
+ORDER BY row_count DESC
